4f272c59787298b31bb15c2eb2220b00813ca943
Julienne Matias <julienne.m93@gmail.com>
2018-10-16 15:06:22 +0200

    Création barre de navigation et footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListEventsComponent } from './list-events/list-events.component';
 import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'events', component: ListEventsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

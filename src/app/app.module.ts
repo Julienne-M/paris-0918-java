c70dd98c12c6eedcdabdc0c03200c893f673b763
Mayeul Lefebvre <mayeul.lefebvre@gmail.com>
2018-10-18 10:28:34 +0200

    service OpenDataParisService intégré au projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListEventsComponent } from './list-events/list-events.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { OpenDataParisServices } from './services/OpenDataParisServices';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -26,9 +28,12 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    OpenDataParisServices
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/OpenDataParisServices.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OpenDataParisServices {
+
+  constructor(private http: HttpClient) { }
+
+  data: any;
+  
+  todaysDate = new Date().toISOString();
+  todaysDateAPIForm = this.todaysDate.slice(0, 10);
+  urlBase = `https://opendata.paris.fr/api/records/1.0/search/?dataset=evenements-a-paris&refine.date_start=${this.todaysDateAPIForm}`;
+  
+  getConcerts() {
+    this.http.get(`${this.urlBase}&refine.tags=concert`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getExpositions() {
+    this.http.get(`${this.urlBase}&refine.tags=exposition`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getTheaters() {
+    this.http.get(`${this.urlBase}&refine.tags=theatre`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getClubbings() {
+    this.http.get(`${this.urlBase}&refine.tags=clubbing`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getShows() {
+    this.http.get(`${this.urlBase}&refine.tags=spectacle`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getCinemas() {
+    this.http.get(`${this.urlBase}&refine.tags=cinema`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getConferences() {
+    this.http.get(`${this.urlBase}&refine.tags=conference`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+
+  getFree() {
+    this.http.get(`${this.urlBase}&refine.pricing_info=gratuit`).subscribe((response) => {
+      this.data = response;
+    });
+    return this.data;
+  }
+}

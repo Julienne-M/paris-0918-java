e3da8e9ff9fd5dee09f64c73e7bd5f4ae0c24afe
Axielles <awacks@gmail.com>
2018-10-25 10:06:33 +0200

    Date au format fr

--- src/app/list-events/list-events.component.ts
+++ src/app/list-events/list-events.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../event';
 import { OpenDataParisServices } from '../services/OpenDataParisServices';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'app-list-events',
@@ -11,6 +12,7 @@ import { OpenDataParisServices } from '../services/OpenDataParisServices';
 export class ListEventsComponent implements OnInit {
   isLoaded = false;
   data: any;
+  frDate: string;
 
   constructor(private api: OpenDataParisServices) {
   }
@@ -20,14 +22,16 @@ export class ListEventsComponent implements OnInit {
       this.data = response;
       // Flag for the ngIf in the HTML
       this.isLoaded = true;
+      // formatage de la date
+      this.frDate = frenchDate();
       // Fliter for the time of Event
       for (let i = 0; i < this.data.records.length; i++) {
         //  Extraction de l'heure
         // this.dateFilter (this.data.records[i].fields.timetable);
         this.data.records[i].fields.timetable = this.data.records[i].fields.timetable.slice(11, 16);
         console.log(this.data);
-        daysDate();
-        daysMonth();
+
+
       }
     });
 
@@ -45,13 +49,25 @@ export class ListEventsComponent implements OnInit {
   }
 }
 
- function daysDate(date = new Date()) {
-   const weekDay = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-   console.log(date.getUTCDay(), weekDay[date.getUTCDay()]);
-}
-
-function daysMonth(month = new Date()) {
-   const monthYear = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre',
+function frenchDate(date = new Date()) {
+  let frDate: string;
+  const space = ' ';
+  const weekDay = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+  const month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre',
   'Octobre', 'Novembre', 'Décembre'];
-  console.log(month.getMonth(), monthYear[month.getMonth()]);
+  // récupération du jour de la semaine
+  frDate = weekDay[date.getUTCDay()] + space;
+
+  // récupération date du jour
+  frDate += date.getUTCDate() + space;
+
+  // récupération du mois de l'année
+  frDate += month[date.getMonth()] + space;
+
+  // Récupération de l'année
+  frDate += date.getFullYear();
+
+  console.log(frDate);
+
+  return frDate;
 }
